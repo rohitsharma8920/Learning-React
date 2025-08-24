8c429e32156cae7f4c84fbbd0914042c8e8adb1b
Rohit Sharma <rsharmaji@7976@gmai.com>
2025-08-24 16:27:44 +0530

    cal-complete

---
+++ projects/calculator-working/src/App.jsx
@@ -0,0 +1,39 @@
+
+import  './App.css'
+import Display from './component/Display'
+import ButtonContainer from './component/ButtonContainer'
+import { useState } from 'react'
+
+function App() {
+
+    const [calVal,setCalVal] = useState("")
+    const onButtonClick = (buttonText) => {
+        console.log(buttonText)
+        if(buttonText === 'C'){
+            setCalVal("")
+
+        }else if(buttonText === '='){
+            const result = eval(calVal)
+            setCalVal(result)
+        }else{
+            const newDisplayValue = calVal +buttonText
+            setCalVal(newDisplayValue)
+        }
+    }
+
+
+
+
+
+ return <>
+ <center>
+ <div className='calculator'>
+  <Display displayValue = {calVal} ></Display>
+
+  <ButtonContainer onButtonClick ={onButtonClick}></ButtonContainer>
+ </div>
+ </center>
+ </>
+}
+
+export default App
---
+++ projects/calculator-working/src/component/Display.jsx
@@ -0,0 +1,12 @@
+import  './Display.css'
+const Display = ({displayValue}) => {
+
+    return(
+        <input type="text" 
+        className='display' 
+        value={displayValue} 
+        readOnly />
+    )
+}
+
+export default Display
